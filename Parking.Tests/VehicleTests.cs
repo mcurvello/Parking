991355d24e7ba42e894a0744255f973a17be9cc4
Marcio Curvello <marciocurvello@gmail.com>
2023-08-06 14:13:57 -0300

    add xunit settings

---
+++ Parking.Tests/VehicleTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Parking.Models;
+using Xunit;
+
+namespace Parking.Tests
+{
+    public class VehicleTests
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Vehicle();
+
+            //Act
+            veiculo.SpeedUp(10);
+
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Vehicle();
+
+            //Act
+            veiculo.Brake(10);
+
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
+
---
+++ Parking.Tests/VehicleTypeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Parking.Models;
+using Xunit;
+
+namespace Parking.Tests
+{
+	public class VehicleTypeTests
+	{
+		[Fact]
+		public void TestaTipoVeiculo()
+		{
+			//Arrange
+			var veiculo = new Vehicle();
+
+			//Act
+
+			//Assert
+			Assert.Equal(VehicleType.Automovel, veiculo.Tipo);
+		}
+	}
+}
+
