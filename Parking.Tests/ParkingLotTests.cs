79afaf656745d7dd1521b1ba796c66f49b13519a
Marcio Curvello <marciocurvello@gmail.com>
2023-08-06 14:26:32 -0300

    add unit tests

---
+++ Parking.Tests/ParkingLotTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Parking.Models;
+using Xunit;
+
+namespace Parking.Tests
+{
+	public class ParkingLotTests
+	{
+		[Fact]
+		public void ValidaFaturamento()
+		{
+			//Arrange
+			var estacionamento = new ParkingLot();
+			var veiculo = new Vehicle();
+
+			veiculo.Proprietario = "Marcio";
+			veiculo.Tipo = VehicleType.Automovel;
+			veiculo.Cor = "Verde";
+			veiculo.Modelo = "Palio";
+			veiculo.Placa = "asd-9999";
+
+			estacionamento.RegistrarEntradaVeiculo(veiculo);
+			estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+			//Act
+			double faturamento = estacionamento.TotalFaturado();
+
+			//Assert
+			Assert.Equal(2, faturamento);
+		}
+	}
+}
+
